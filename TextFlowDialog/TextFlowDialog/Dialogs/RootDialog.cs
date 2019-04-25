49073cd5f151e9e39e406527df12a1314dc8cc85
Sergio Flores Ramírez <serchfr890@outlook.es>
2019-04-25 00:59:27 -0500

    Falta por integrar LUIS en el código

---
+++ TextFlowDialog/TextFlowDialog/Dialogs/RootDialog.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Bot.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TextFlowDialog.Dialogs
+{
+    public class RootDialog
+    {
+        public static async Task MainDialog(ITurnContext turnContext)
+        {
+            await turnContext.SendActivityAsync("Hola Te estoy mandando este mensaje desde mainDialog");
+        }
+    }
+}
